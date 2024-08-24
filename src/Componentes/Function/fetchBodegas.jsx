c389525dd804b02c0c61d75293fa78276cafd08a
Jkz2025 <joeldavidvasquezrivas@gmail.com>
2024-08-24 16:34:52 -0500

    first commit

--- src/Componentes/Function/fetchBodegas.jsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from "../../CreateClient";
-
-export async function fetchBodegas() {
-  const { data, error } = await supabase.from("bodegas").select("*");
-  if (error) {
-    throw new Error(error.message);
-  }
-  return data;
-}
--- src/Componentes/Function/fetchEmpresa.jsx
+++
@@ -1,9 +0,0 @@
-import { supabase } from "../../CreateClient";
-
-export async function fetchEmpresa() {
-  const { data, error } = await supabase.from("empresa").select("*");
-  if (error) {
-    throw new Error(error.message);
-  }
-  return data;
-}
